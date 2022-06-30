ba62d1666e227c0a6f11c9af401f0eb45ed2d037
Yuval Einat <yuval.einat@MAC-YUVALEI.local>
2022-06-30 12:01:41 +0300

    fixing bad indentation

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -1,14 +1,16 @@
 from random import choices
 
+
 def fruit():
     fruits = ["apple", "cherry", "strawberry"]
     return choices(fruits)[0]
 
+
 def meal(beverage):
     my_fruit = fruit()
-    print(f'Your fruit is {my_fruit} and your beverage is {beverage}')
+    print(f"Your fruit is {my_fruit} and your beverage is {beverage}")
     if my_fruit == "cherry":
-        complete_meal = f'Your meal is a {beverage} and {my_fruit}'
+        complete_meal = f"Your meal is a {beverage} and {my_fruit}"
         return complete_meal
-    alternate_meal = f'Your meal is a {beverage} and a steak'
+    alternate_meal = f"Your meal is a {beverage} and a steak"
     return alternate_meal
